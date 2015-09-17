df567eada3178a0b39642bbde4734a69b556a1db
Martina Sandberg <Martini@c-212-90.eduroam.liu.se>
2015-09-17 13:18:40 +0200

    Linreg updated

--- RLab4/R/linregtest.R
+++ RLab4/R/linregtest.R
@@ -3,7 +3,8 @@ linreg <- setRefClass("linreg",
                       fields=list(formula="formula",data="data.frame",
                                   betaHat="matrix",yHat="matrix",res="matrix",
                                   df="integer", varRes="numeric", 
-                                  varCoef="matrix",tBeta2="numeric",p="numeric"),
+                                  varCoef="matrix",tBeta2="numeric",p="numeric",
+                                  rstand2="numeric",cstand2="numeric"),
                       methods=list(
                         initialize = function(formula,data) {
                           X<-model.matrix(formula, data=data)
@@ -21,6 +22,17 @@ linreg <- setRefClass("linreg",
                           }
                           tBeta2<<-tBeta
                           p<<-2*pt(abs(tBeta2), df=df, lower.tail=FALSE)
+                          rstand<-numeric(0)
+                          for (i in seq(length(res))) {
+                            rstand[i]<-sqrt((abs(res[i]-mean(res)))/varRes)
+                          }
+                          rstand2 <<- rstand
+                          cstand<-numeric(0)
+                          for (i in seq(length(betaHat))) {
+                            cstand[i]<-sqrt(varCoef[i,i])
+                          }
+                          cstand2 <<- cstand
+                          library(ggplot2)
                         },
                         coef = function() {
                           betaHat
@@ -35,24 +47,46 @@ linreg <- setRefClass("linreg",
                           t(betaHat)
                         },
                         plot = function() {
-                          data2 <- data.frame(yHat,res)
+                          data2 <- data.frame(yHat,res,rstand2)
+                          
                           pl1 <- 
                             ggplot(data2) + 
                             aes(x=yHat, y=res) + 
-                            geom_point()
-                          return(pl1)
+                            geom_point(shape = 1) +
+                            geom_smooth(method = "loess", formula = y ~ x, se = FALSE, colour = "red") +
+                            labs(x = "Fitted values", y = "Residuals") + 
+                            ggtitle("Residuals vs Fitted") +
+                            theme_bw() +
+                            theme(panel.grid.major = element_blank(),
+                                  panel.grid.minor = element_blank(),
+                                  plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5))
+                          
+                          
+                          pl2 <- 
+                            ggplot(data2) + 
+                            aes(x=yHat, y=rstand2) + 
+                            geom_point(shape = 1)+
+                            geom_smooth(method = "loess", formula = y ~ x, se = FALSE, colour = "red") +
+                            labs(x = "Fitted values", y = "sqrt(Standard residuals)") + 
+                            ggtitle("Scale-Location") +
+                            theme_bw() +
+                            theme(panel.grid.major = element_blank(),
+                                  panel.grid.minor = element_blank(),
+                                  plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5))
+
+                          return(list(pl1,pl2))
                         },
                         summary = function() {
-                          s <- data.frame(betaHat,diag(varCoef),tBeta2,p)
-                          return(s)
-                          df
-                          return(varRes)
+                          s <- data.frame(betaHat,cstand2,tBeta2,p)
+                          colnames(s) <- c("Estimate", "Std.Error","t value","p value")
+                          
+                          return(list(Coefficients=s,degrees_of_freedom=df,residual_standard_error=sqrt(varRes)))
                         }))
 
 
 
 a <- linreg(Sepal.Length ~ Petal.Length,iris)
-a$coef()
+a$plot()
 
 b <- lm(Sepal.Length ~ Petal.Length,iris)
 
