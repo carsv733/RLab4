c1e7623ab24f72a6410edb731828cd72bc55c016
carsv733 <carsv733@student.liu.se>
2015-10-09 10:13:18 +0200

    Added interations

--- RLab4/R/test.R
+++ RLab4/R/test.R
@@ -1,7 +1,15 @@
 data(weather)
 data(flights)
 require(dplyr)
+install.packages("caret")
+library(caret)
+install.packages("mlbench")
+library(mlbench)
+data(BostonHousing)
+data(oil)
 
 flights<-select(flights, year, month, day, hour, origin, dep_delay, arr_delay, distance)
-weather<-select(weather, -dewp, -humid, -precip, -pressure)
-wfComb<-flights %>% inner_join(weather, by=c("origin", "year", "month", "day", "hour")) 
+weather<-select(weather, -dewp, -humid, -pressure)
+wfComb<-flights %>% inner_join(weather, by=c("origin", "year", "month", "day", "hour")) %>% mutate("prec*wind"=precip*wind_speed, "dir*wind"=wind_dir*wind_speed) 
+
+
