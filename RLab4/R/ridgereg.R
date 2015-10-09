2169e78a05db46ce64b001aadaf91a8566f36ab7
Martina Sandberg <Martini@c-213-34.eduroam.liu.se>
2015-10-09 11:38:58 +0200

    Extended Ridgereg and 1.2 vignette

--- RLab4/R/ridgereg.R
+++ RLab4/R/ridgereg.R
@@ -1,7 +1,9 @@
 
 ridgereg <- setRefClass("ridgereg",
                         fields=list(formula="formula",data="data.frame",lambda="numeric",
-                                    betaHat="matrix",yHat="matrix"),
+                                    betaHat="matrix",yHat="matrix",df="integer", varRes="numeric", 
+                                    varCoef="matrix",tBeta2="numeric",p="numeric",res="matrix",
+                                    rstand2="numeric",cstand2="numeric"),
                         methods=list(
                           initialize = function(formula,data,lambda) {
                             
@@ -23,14 +25,78 @@ ridgereg <- setRefClass("ridgereg",
                             betaHat<<-solve(t(X_norm)%*%X_norm +  diag(rep(lambda,dim(X_norm)[2]), dim(X_norm)[2])   )%*%t(X_norm)%*%y
                             yHat<<-X_norm%*%betaHat
                             
+                            res<<-y-yHat
+                            df<<-length(y)-dim(betaHat)[1]
+                            varRes<<-as.numeric((t(res)%*%res)/df)
+                            varCoef<<-varRes*(solve(t(X)%*%X))
+                            tBeta<-matrix()
+                            for (i in seq(dim(betaHat)[1])) {
+                              tBeta[i]<-betaHat[i]/sqrt(varCoef[i,i])
+                            }
+                            tBeta2<<-tBeta
+                            p<<-2*pt(abs(tBeta2), df=df, lower.tail=FALSE)
+                            rstand<-numeric(0)
+                            for (i in seq(length(res))) {
+                              rstand[i]<-sqrt((abs(res[i]-mean(res)))/varRes)
+                            }
+                            rstand2 <<- rstand
+                            cstand<-numeric(0)
+                            for (i in seq(length(betaHat))) {
+                              cstand[i]<-sqrt(varCoef[i,i])
+                            }
+                            cstand2 <<- cstand
+                            library(ggplot2)
                           },
                           coef = function() {
                             betaHat
+                          },
+                          resid = function() {
+                            res
                           },
                           predict = function() {
                             yHat
                           },
                           print = function() {
-                            list(Coefficients=t(betaHat))
+                            t(betaHat)
+                          },
+                          plot = function() {
+                            data2 <- data.frame(yHat,res,rstand2)
+                            
+                            pl1 <- 
+                              ggplot(data2) + 
+                              aes(x=yHat, y=res) + 
+                              geom_point(shape = 1) +
+                              geom_smooth(method = "loess", formula = y ~ x, se = FALSE, colour = "red") +
+                              labs(x = "Fitted values", y = "Residuals") + 
+                              ggtitle("Residuals vs Fitted") +
+                              theme_bw() +
+                              theme(panel.grid.major = element_blank(),
+                                    panel.grid.minor = element_blank(),
+                                    plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5))
+                            
+                            
+                            pl2 <- 
+                              ggplot(data2) + 
+                              aes(x=yHat, y=rstand2) + 
+                              geom_point(shape = 1)+
+                              geom_smooth(method = "loess", formula = y ~ x, se = FALSE, colour = "red") +
+                              labs(x = "Fitted values", y = "sqrt(Standard residuals)") + 
+                              ggtitle("Scale-Location") +
+                              theme_bw() +
+                              theme(panel.grid.major = element_blank(),
+                                    panel.grid.minor = element_blank(),
+                                    plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5))
+                            devAskNewPage(ask = TRUE)
+                            return(list(pl1,pl2))
+                            
+                          },
+                          summary = function() {
+                            s <- data.frame(betaHat,cstand2,tBeta2,p)
+                            colnames(s) <- c("Estimate", "Std.Error","t value","p value")
+                            
+                            return(list(Coefficients=s,degrees_of_freedom=df,residual_standard_error=sqrt(varRes)))
                           }))
+
+#a <- ridgereg(Sepal.Length ~ Petal.Width + Petal.Length, iris, 0.2)
+      
 
--- RLab4/vignettes/invignette.R
+++ RLab4/vignettes/invignette.R
@@ -1,6 +1,7 @@
 library(RLab4)
 library(mlbench)
 library(caret)
+data(BostonHousing)
 
 trainIndex <- createDataPartition(BostonHousing$medv, p = .8, list = FALSE,times = 1)
 
@@ -17,6 +18,7 @@ model1 <- train(medv ~ crim + indus + zn + chas +
 model2 <- train(medv ~ crim + indus + zn + chas + 
                   nox + rm + age + dis + rad + tax + ptratio 
                 + b + lstat,data=Train,method = 'leapForward')
+model2 <- train(medv ~ rm +  ptratio + lstat, data=Train,method = 'lm')
 
 summary(model1)
 summary(model2)
@@ -28,3 +30,72 @@ names(model_info)
 
 
 
+modelInfo <- list(label = "Ridge Regression",
+                  library = "RLab4",
+                  type = "Regression",
+                  parameters = data.frame(parameter = c('lambda'),
+                                          class = c("numeric"),
+                                          label = c('Weight Decay')),
+                  grid = function(x, y, len = NULL, search = "grid")  {
+                    if(search == "grid") {
+                      out <- expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)))
+                    } else {
+                      out <- data.frame(lambda = 10^runif(len, min = -5, 1))
+                    }
+                    out
+                  },
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    enet(as.matrix(x), y, lambda = param$lambda)  
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    predict(modelFit, newdata, 
+                            s = 1, 
+                            mode = "fraction")$fit
+                  },
+                  predictors = function(x, s = NULL, ...) {
+                    if(is.null(s))
+                    {
+                      if(!is.null(x$tuneValue))
+                      {
+                        s <- x$tuneValue$.fraction
+                      } else stop("must supply a vaue of s")
+                      out <- predict(x, s = s,
+                                     type = "coefficients",
+                                     mode = "fraction")$coefficients
+                      
+                    } else {
+                      out <- predict(x, s = s)$coefficients
+                      
+                    }
+                    names(out)[out != 0]
+                  },
+                  tags = c("Linear Regression", "L2 Regularization"),
+                  prob = NULL,
+                  sort = function(x) x[order(-x$lambda),])
+
+
+
+
+model3 <- train(medv ~ crim + indus + zn + chas + 
+                  nox + rm + age + dis + rad + tax + ptratio 
+                + b + lstat,data=Train,
+                   method = modelInfo)
+
+
+fitControl <- trainControl(method = "repeatedcv",
+                           ## 10-fold CV...
+                           number = 10,
+                           ## repeated ten times
+                           repeats = 10)
+
+model4 <- train(medv ~ crim + indus + zn + chas + 
+                  nox + rm + age + dis + rad + tax + ptratio 
+                + b + lstat,data=Train,
+                method = modelInfo,
+                trControl = fitControl)
+model5 <- ridgereg(medv ~ crim + indus + zn + chas + 
+           nox + rm + age + dis + rad + tax + ptratio 
+         + b + lstat,data=Train,0.0001)
+
+model5$summary()
