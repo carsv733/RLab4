8e3dc5b96771e66cb77d410cc2831e796a41ee08
Caroline Svahn <carsv14@liu.se>
2019-02-12 10:36:31 +0100

    Bug fix - previous test was skipped due to usage of outdated function in the testthat package. Updated with the new function and the test now works.

--- RLab4/tests/testthat/test_coef.R
+++ RLab4/tests/testthat/test_coef.R
@@ -2,7 +2,7 @@ test_that("Return vector of named coefficients", {
   data(iris)
   model1 <- linreg(Sepal.Length ~ Petal.Length, data=iris)
   model2 <- lm(Sepal.Length ~ Petal.Length, data=iris)
-  all.equal(coefficients(model2), is_equivalent_to(model1$coef()))
+  expect_equivalent(coefficients(model2), model1$coef())
 })
 
 
