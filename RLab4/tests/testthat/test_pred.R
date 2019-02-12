ed0c77943d872ff13e5983c29afff608d557e0a8
Caroline Svahn <carsv14@liu.se>
2019-02-12 10:40:27 +0100

    Bux fix of prediction test - outdated function from testthat package replaced with the current function, and the test now works.

--- RLab4/tests/testthat/test_pred.R
+++ RLab4/tests/testthat/test_pred.R
@@ -2,5 +2,5 @@ test_that("Return vector of predicted values yhat", {
   data(iris)
   model1 <- linreg(Sepal.Length ~ Petal.Length, data=iris)
   model2 <- lm(Sepal.Length ~ Petal.Length, data=iris)
-  all.equal(fitted.values(model2), is_equivalent_to(model1$pred()))
+  expect_equivalent(fitted.values(model2), model1$pred())
 })
