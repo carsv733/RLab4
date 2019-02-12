268738abb8aa3ac8463ab70f4f846bdc3c25d84e
Caroline Svahn <carsv14@liu.se>
2019-02-12 10:41:26 +0100

    Bux fix of residual test - outdated function from testthat package replaced with the current function, and the test now works.

--- RLab4/tests/testthat/test_resid.R
+++ RLab4/tests/testthat/test_resid.R
@@ -2,5 +2,5 @@ test_that("Return vector of residuals", {
   data(iris)
   model1 <- linreg(Sepal.Length ~ Petal.Length, data=iris)
   model2 <- lm(Sepal.Length ~ Petal.Length, data=iris)
- all.equal(residuals(model2), is_equivalent_to(model1$resid()))
+  expect_equivalent(residuals(model2), model1$resid())
 })
