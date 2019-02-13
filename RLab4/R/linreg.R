0874355fcf01c7866fb49660c0eb5fcb9aa5029b
Caroline Svahn <carsv14@liu.se>
2019-02-13 10:29:41 +0100

    Removal of obsolete comments

--- RLab4/R/linreg.R
+++ RLab4/R/linreg.R
@@ -1,4 +1,3 @@
-#Commit test comment
 linreg <- setRefClass("linreg",
                       fields=list(formula="formula",data="data.frame",
                                   betaHat="matrix",yHat="matrix",res="matrix",
@@ -84,22 +83,4 @@ linreg <- setRefClass("linreg",
                           return(list(Coefficients=s,degrees_of_freedom=df,residual_standard_error=sqrt(varRes)))
                         }))
 
-#linreg <- function(formula, data) {
-#  X<-model.matrix(formula, data=data)
-#  nameY<-all.vars(formula)[1]
-#  y<-data[[nameY]] 
-#  betaHat<-solve(t(X)%*%X)%*%t(X)%*%y
-#  yHat<-X%*%betaHat
-#  res<-y-yHat  
-#  df<-length(y)-dim(betaHat)[1] 
-#  varRes<-as.numeric((t(res)%*%res)/df) 
-#  varCoef<-varRes*(solve(t(X)%*%X)) 
-#  tBeta<-matrix()
-#  for (i in seq(dim(betaHat)[1])) {
-#    tBeta[i]<-betaHat[i]/sqrt(varCoef[i,i])
-#  } 
-#  p<-2*pt(abs(tBeta), df=df, lower.tail=FALSE)
-#  return(list(betaHat, yHat, res, df, varRes, varCoef, tBeta, p)) 
-#}
-#linreg(waiting ~ eruptions, faithful)
 
