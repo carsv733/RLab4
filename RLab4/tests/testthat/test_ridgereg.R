c062ef109d9b4fde9811cc0afa08a5d9bb8f4184
Caroline Svahn <carsv14@liu.se>
2019-02-13 13:54:23 +0100

    Added input control tests to ensure the function thrrows the correct messages and errors.

--- RLab4/tests/testthat/test_ridgereg.R
+++ RLab4/tests/testthat/test_ridgereg.R
@@ -77,3 +77,27 @@ test_that("function returns standard deviation of coefficients as a numeric", {
   model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
   expect_that(model$cstand2, is_a("numeric"))
 })
+
+#Message tests
+test_that("function returns a message when not fed any data", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_message(model$predict(), "No data provided, using original input.")
+})
+
+test_that("function throws an error when not fed a formula", {
+  data(iris)
+  form <- "Sepal.Length ~ Petal.Length+Petal.Width"
+  expect_error(ridgereg(form,iris,lambda=0.5), "The formula provided is not a formula.")
+})
+
+test_that("function throws an error when not fed a data frame", {
+  data(iris)
+  expect_error(ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,as.list(iris),lambda=0.5), "The data provided is not a data frame.")
+})
+
+test_that("function throws an error when not fed a numeric lambda", {
+  data(iris)
+  expect_error(ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda="0.5"), "The lambda provided is not a numeric.")
+})
+
