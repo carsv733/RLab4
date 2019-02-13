624deb19d46da79ae0c0825a205a0e9e806c9f47
Caroline Svahn <carsv14@liu.se>
2019-02-13 11:18:41 +0100

    Added tests to check that the function returns the correct object types.

--- RLab4/tests/testthat/test_ridgereg.R
+++ RLab4/tests/testthat/test_ridgereg.R
@@ -7,7 +7,73 @@ test_that("Return right class", {
 
 
 test_that("Insert right object", {
-  formula <- Sepal.Length ~ Petal.Length
-  expect_that(formula, is_a("formula"))
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$formula, is_a("formula"))
 })
 
+test_that("function returns input data as a data frame", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$data, is_a("data.frame"))
+})
+
+test_that("function returns coefficients as a matrix", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$betaHat, is_a("matrix"))
+})
+
+test_that("function returns predictions as a matrix", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$yHat, is_a("matrix"))
+})
+
+test_that("function returns residuals as a matrix", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$res, is_a("matrix"))
+})
+
+test_that("function returns degrees of freedom as an integer", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$df, is_a("integer"))
+})
+
+test_that("function returns residual variance as a numeric", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$varRes, is_a("numeric"))
+})
+
+test_that("function returns coefficient variance as a matix", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$varCoef, is_a("matrix"))
+})
+
+test_that("function returns normalized coefficients as a numeric", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$tBeta2, is_a("numeric"))
+})
+
+test_that("function returns p-value as a numeric", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$p, is_a("numeric"))
+})
+
+test_that("function returns standard deviation of residuals as a numeric", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$rstand2, is_a("numeric"))
+})
+
+test_that("function returns standard deviation of coefficients as a numeric", {
+  data(iris)
+  model <- ridgereg(Sepal.Length ~ Petal.Length+Petal.Width,iris,lambda=0.5)
+  expect_that(model$cstand2, is_a("numeric"))
+})
