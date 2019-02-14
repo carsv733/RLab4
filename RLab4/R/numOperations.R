978ea191b06d265c3f788c7223315acf16243c4e
Caroline Svahn <carsv14@liu.se>
2019-02-14 15:30:57 +0100

    Rename field - renamed X in class to avoid confusion.

--- RLab4/R/numOperations.R
+++ RLab4/R/numOperations.R
@@ -1,14 +1,15 @@
 
-numOperations <- setRefClass("numOperations", fields=c(X="matrix", betaHat="matrix"), methods=list(
+numOperations <- setRefClass("numOperations", fields=c(X_in="matrix", betaHat="matrix"), methods=list(
   norm_x = function(X){
+    X_new<-X
     for (i in 2:dim(X)[2]){
       mean <- mean(X[,i])
       sd <- sd(X[,i])
       for (j in 1:dim(X)[1]) {
-        X[j,i] <- (X[j,i]-mean)/sd 
+        X_new[j,i] <- (X[j,i]-mean)/sd 
       }
     }
-    return(X)
+    return(X_new)
   },
   pred_y = function(X,betaHat) {
     yHat <- X%*%betaHat
